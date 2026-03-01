d13b7e6ac12780d9421c0d8674e545fa016c5d79
Dan Theriault <dan@theriault.codes>
2026-02-28 21:26:21 -0500

    Configure blink-cmp-git

--- config/cmp.nix
+++ config/cmp.nix
@@ -9,11 +9,26 @@
           "path"
           "snippets"
           "buffer"
+          "git"
         ];
+        providers = {
+          git = {
+            module = "blink-cmp-git";
+            name = "git";
+            score_offset = 100;
+            opts = {
+              commit = { };
+              git_centers = {
+                git_hub = { };
+              };
+            };
+          };
+        };
       };
       snippets.preset = "luasnip";
       fuzzy.implementation = "lua";
       signature.enabled = true;
     };
   };
+  plugins.blink-cmp-git.enable = true;
 }
